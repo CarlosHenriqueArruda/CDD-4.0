02c1471e865ea03d4c9c7f5bc15d6a12d132d313
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-10 14:45:50 -0300

    Add files via upload

---
+++ Python/Exercicio Python 1.py
@@ -0,0 +1,44 @@
+#Nesse codigo aprendi a utilizar a função "DEF" para definir uma função# Criei uma calculadora simples#
+opcao = 0
+def somar(n1,n2):
+    soma = n1+n2
+    print(f"O resultado da soma é: {soma}")
+    print()
+def subtrair(n1,n2):
+    subtracao = n1-n2
+    print(f"O resultado da subtração é: {subtrair}")
+    print()
+def multiplicar(n1,n2):
+    multiplicacao = n1*n2
+    print(f"O resultado da multiplicação é: {multiplicar}")
+    print()
+def dividir(n1,n2):
+    dividisao = n1/n2
+    print(f"O resultado da divisão é: {dividir}")
+    print()
+
+while opcao != 5:
+    escolha = int(input("Escolha uma opção: \nSomar - 1\nSubtrair - 2 \nMultiplicar - 3 \nDividir - 4 \nSair - 5 \nSua opção: "))
+    print()
+    if escolha == 1:
+        a = float(input("Digite o primeiro numero: "))
+        b = float(input("Digite o segundo numero: "))
+        print()
+        somar(a,b)
+    if escolha == 2:
+        a = float(input("Digite o primeiro numero: "))
+        b = float(input("Digite o segundo numero: "))
+        print()
+        subtrair(a,b)
+    if escolha == 3:
+        a = float(input("Digite o primeiro numero: "))
+        b = float(input("Digite o segundo numero: "))
+        print()
+        multiplicar(a,b)
+    if escolha == 4:
+        a = float(input("Digite o primeiro numero: "))
+        b = float(input("Digite o segundo numero: "))
+        print()
+        dividir(a,b)
+    if escolha == 5:
+        break
