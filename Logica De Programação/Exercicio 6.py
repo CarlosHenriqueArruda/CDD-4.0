feb9d06e543f9217c38461642801bf640bf138c6
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-12 13:57:32 -0300

    Delete Exercicio 6.py

--- Logica De Programação/Exercicio 6.py
+++
@@ -1,9 +0,0 @@
-#While = enquanto - condição (ex: enquanto tiver cadeiras, pode entrar)
-lista = 1
-contador = 0
-while lista <=10:
-    numero = float(input("Digite o valor:"))
-    contador += numero
-    media = contador / 10
-    lista += 1
-print(media)
