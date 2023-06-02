a5ef1fe54df1d7af1664fcb012a54dd006bf9fac
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-06-02 15:42:22 -0300

    Atleta.py

--- Python/Programação Orientada a Objetos/Atleta.py
+++ Python/Programação Orientada a Objetos/Atleta.py
@@ -1,6 +1,7 @@
 CORRER = 0
 NADAR = 1
 PEDALAR = 2
+
 class Atleta:
     def __init__(self, peso):
         self.aposentado = False
@@ -13,48 +14,50 @@ class Atleta:
             print("Está aposentado.")
         else:
             print("Já está aposentado!")
+
     def Desaposentar(self):
         if self.aposentado == True:
             self.aposentado = False
             print("Está desaposentado.")
         else:
             print("Já está desaposentado!")
+
     def Aquecer(self):
         if not self.aquecido and not self.aposentado:
             self.aquecido = True
-            print(f"Está aquecendo!")
+            print("Está aquecendo!")
         else:
             print("Já está aquecido")
 
 class Corredor(Atleta):
-    def __init__(self,peso):
+    def __init__(self, peso):
         super().__init__(peso)
+
     def Correr(self):
         if not self.aposentado and self.aquecido:
             print("Começou a correr!")
         else:
             print("Está aposentado ou não aqueceu!")
 
-
 class Nadador(Atleta):
-    def __init__(self,peso):
+    def __init__(self, peso):
         super().__init__(peso)
+
     def Nadar(self):
         if not self.aposentado and self.aquecido:
             print("Começou a nadar!")
         else:
             print("Está aposentado ou não aqueceu!")
 
-
 class Ciclista(Atleta):
-    def __init__(self,peso):
+    def __init__(self, peso):
         super().__init__(peso)
+
     def Pedalar(self):
         if not self.aposentado and self.aquecido:
             print("Começou a pedalar!")
         else:
             print("Está aposentado ou não aqueceu!")
-
 
 class TriAtleta(Corredor, Nadador, Ciclista):
     def __init__(self, peso):
@@ -63,28 +66,39 @@ class TriAtleta(Corredor, Nadador, Ciclista):
 
     def Correr(self):
         if not self.aposentado and self.aquecido:
-            if self.atividade_atual is None or self.atividade_atual == PEDALAR or self.atividade_atual == NADAR:
+            if self.atividade_atual is None:
                 self.atividade_atual = CORRER
                 print("Começou a correr!")
+            elif self.atividade_atual == NADAR or self.atividade_atual == PEDALAR:
+                print("Só pode realizar uma atividade por vez. Pare a atividade atual e tente novamente.")
         else:
             print("Está aposentado ou não aqueceu!")
 
     def Nadar(self):
         if not self.aposentado and self.aquecido:
-            if self.atividade_atual is None or self.atividade_atual == PEDALAR or self.atividade_atual == CORRER:
+            if self.atividade_atual is None:
                 self.atividade_atual = NADAR
                 print("Começou a nadar!")
+            elif self.atividade_atual == CORRER or self.atividade_atual == PEDALAR:
+                print("Só pode realizar uma atividade por vez. Pare a atividade atual e tente novamente.")
         else:
             print("Está aposentado ou não aqueceu!")
 
     def Pedalar(self):
         if not self.aposentado and self.aquecido:
-            if self.atividade_atual is None or self.atividade_atual == CORRER or self.atividade_atual == NADAR:
+            if self.atividade_atual is None:
                 self.atividade_atual = PEDALAR
                 print("Começou a pedalar!")
+            elif self.atividade_atual == CORRER or self.atividade_atual == NADAR:
+                print("Só pode realizar uma atividade por vez. Pare a atividade atual e tente novamente.")
         else:
             print("Está aposentado ou não aqueceu!")
 
+    def Parar(self):
+        if not self.aposentado and self.aquecido:
+            if self.atividade_atual is not None:
+                self.atividade_atual = None
+                print("O atleta parou!")
 
 carlos = TriAtleta(65)
 carlos.Aquecer()
